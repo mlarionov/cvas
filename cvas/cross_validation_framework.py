d22fa771f45c5ff8726bb0ee60c5fd1fc9eeb468
Michael Larionov <6360814+mlarionov@users.noreply.github.com>
2022-05-07 16:29:20 -0500

    Generalized the framework to work with any classification problem

--- cvas/cross_validation_framework.py
+++ cvas/cross_validation_framework.py
@@ -1,13 +1,13 @@
 from abc import abstractmethod
 from statistics import mean, stdev
-from typing import Tuple
+from typing import Tuple, Literal, Callable, List
 
-import optuna
-from optuna import Trial
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import StratifiedKFold
 import numpy as np
+import optuna
 import pandas as pd
+from numpy.typing import ArrayLike
+from optuna import Trial
+from sklearn.model_selection import StratifiedKFold, KFold
 
 
 class ModelGenerator:
@@ -41,36 +41,45 @@ class ModelGenerator:
 class KFoldCrossValidator:
 
     def __init__(self,
-                 X,
-                 y,
-                 n_splits,
-                 n_repeats,
-                 random_state,
+                 X: ArrayLike,
+                 y: ArrayLike,
+                 n_splits: int,
+                 n_repeats: int,
+                 random_state: int,
+                 metric: Callable[[ArrayLike, ArrayLike], float],
+                 stratify: Literal["yes", "no", "auto"] = "auto",
                  columns=None):
 
+        """
+        Perform cross-validation
+        :param X: predictor variables (most likely, a data frame)
+        :param y: target variable
+        :param n_splits: number of splits in K-Fold
+        :param n_repeats: number of times we repeat K-Fold validation for a different random state
+        :param random_state: initial random state
+        :param stratify: Yes, no, auto: indicates if we need to stratify when doing a K-fold split
+            by default we enable it for a binary classification problem
+        :param columns: use these to create pandas DataFrames
+        :return: an array with the accuracy results
+        """
         self.X = X
         self.y = y
         self.n_splits = n_splits
         self.n_repeats = n_repeats
         self.random_state = random_state
         self.columns = columns
+        self.metric = metric
+        is_binary_classification = len(np.unique(self.y)) < 3
+        if stratify == "yes" or stratify == "auto" and is_binary_classification:
+            self.generator_function = StratifiedKFold
+        elif stratify == "no" or stratify == "auto" and not is_binary_classification:
+            self.generator_function = KFold
 
-    """
-    Perform cross-validation
-    :param model_generating_function: model
-    :param X: predictor variables (most likely, a data frame)
-    :param y: target variable
-    :param n_splits: number of splits in K-Fold
-    :param n_repeats: number of times we repeat K-Fold validation for a different random state
-    :param random_state: initial random state
-    :param columns: use these to create pandas DataFrames
-    :return: an array with the accuracy results
-    """
 
-    def validate(self, model_generator: ModelGenerator, parameters):
+    def validate(self, model_generator: ModelGenerator, parameters) -> List[float]:
         results = []
         for state in range(self.random_state, self.random_state + self.n_repeats):
-            generator = StratifiedKFold(n_splits=self.n_splits, shuffle=True, random_state=state)
+            generator = self.generator_function(n_splits=self.n_splits, shuffle=True, random_state=state)
             for train_index, test_index in generator.split(self.X, self.y):
                 X_arr = np.array(self.X)
                 X_train, X_test = X_arr[train_index], X_arr[test_index]
@@ -82,18 +91,35 @@ class KFoldCrossValidator:
 
                 model = model_generator.get_model(parameters, state)
                 model.fit(X_train, y_train)
-                results.append(accuracy_score(y_test, model.predict(X_test)))
+                results.append(self.metric(y_test, model.predict(X_test)))
         return results
 
 
 class HyperParameterTuner:
 
-    def __init__(self, cross_validator: KFoldCrossValidator, model_generator: ModelGenerator):
+    def __init__(
+            self,
+            cross_validator: KFoldCrossValidator,
+            model_generator: ModelGenerator,
+            direction: Literal["maximize", "minimize"] = "maximize"
+    ):
+        """
+        Tune hyperparameters
+        :param cross_validator: an instance of cross-validator
+        :param model_generator: an instance of model generator
+        :param direction: (maximize or minimize) direction to use when optimizing the objective:
+        """
         self.cross_validator = cross_validator
         self.model_generator = model_generator
+        self.direction = direction
         self.trial_results = {}
 
     def objective(self, trial: Trial):
+        """
+        Run the trial one time, average the metric
+        :param trial: Optuna trial
+        :return: mean of the metric
+        """
         parameters = self.model_generator.suggest_parameters(trial)
 
         results = self.cross_validator.validate(self.model_generator, parameters)
@@ -101,6 +127,11 @@ class HyperParameterTuner:
         return mean(results)
 
     def study(self, n_trials: int) -> Tuple[float, float, dict]:
-        study = optuna.create_study(direction="maximize")
+        """
+        Conducts an Optuna study
+        :param n_trials: Number of trials
+        :return: mean and std of the metric for the best model.
+        """
+        study = optuna.create_study(direction=self.direction)
         study.optimize(self.objective, n_trials=n_trials, )
         return study.best_trial.value, stdev(self.trial_results[study.best_trial.number]), study.best_params
